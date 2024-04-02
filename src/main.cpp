7a141a65b7179e302d939177b15a95a60a7be224
kkrowland <kkrowland@live.com>
2024-04-02 10:27:36 -0700

    First Commit
    Getting OpenGL set up in my dev environment

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <format>
+#include <fstream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  
+    return 0;
+}
